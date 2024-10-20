447d26be6ad913f5d0c4496f5b69be98d62f955e
Jayden <jayden.pearce12@gmail.com>
2024-10-20 09:23:28 +0200

    added apex chart library and created a pie chart demonstrating sales by order types.

---
+++ SalesOrders/Client/wwwroot/js/analytics.js
@@ -0,0 +1,24 @@
+﻿function renderChart(data) {
+    var options = {
+        series: data.series, // Use the data passed from Blazor
+        chart: {
+            width: 380,
+            type: 'pie',
+        },
+        labels: data.labels, // Use labels passed from Blazor
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    chart.render();
+}
